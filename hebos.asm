bfc76b33fd2bb28a8f48ea212213c4eaca4e285b
Ryutaa <osakabishop@gmail.com>
2017-11-10 22:45:29 +0900

    Code descriptorとData descriptorを設定
    プロテクトモードに入り、セグメントレジスタでディスクリプタを設定。まだ動作テストはしていない。

--- hebos.asm
+++ hebos.asm
@@ -6,8 +6,8 @@ BITS 16
     MOV EAX, CR0
     OR EAX, 1
     MOV CR0, EAX
-    JMP reset_pipeline
-reset_pipeline:
+    JMP 0x08:Protected_Mode     ;Code descriptor(0x08)をセレクト
+
     
 
 gdt_info:
@@ -36,3 +36,15 @@ _gdt:					;GDT
     DB 10010010b		; P, DPL(6 ~ 5), S, Type(3 ~ 0)
     DB 11001111b		; G, D/B, AVL (53 ~ 52), Segment Limit High(51 ~ 48)
     DB 0x00			; Base Address High	
+
+BITS 32
+Protected_Mode:
+    ;Data descriptorをセレクト
+    MOV AX, 0x10
+    MOV SS, AX
+    MOV ES, AX
+    MOV FS, AX
+    MOV GS, AX
+    MOV DS, AX
+
+    MOV ESP, 90000h     ;ESPの値は考え直した方がいいかも
